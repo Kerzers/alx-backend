4dd93608d5373d673b33fa8ae8d6d15b119f3bcd
kerzers <ahra.ahlame@gmail.com>
2024-02-07 13:35:21 +0100

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app"""
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, gettext
 
 
 class Config(object):
@@ -25,7 +25,7 @@ def get_locale():
 @app.route('/')
 def index() -> str:
     """handles / route"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
